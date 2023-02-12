aa7a7c4047e2a11c7b7e9da3e7ae27a332c5ca25
cloydlau <253055734@qq.com>
2023-02-12 21:14:34 +0800

    chore: code formatting

--- src/Component.ts
+++ src/Component.ts
@@ -110,7 +110,8 @@ export default defineComponent({
             menubar: 'file edit view insert format tools table help',
             toolbar:
               'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
-            quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+            quickbars_selection_toolbar:
+              'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
             contextmenu: 'link image table',
             branding: false,
             promotion: false,
